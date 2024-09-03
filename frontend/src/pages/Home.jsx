f8736c5e98ebbf03f425aa6763a19254bbff0e4c
Arkaprava23 <arkaprava.digitalexpeditor@gmail.com>
2024-09-03 19:22:31 +0530

    setup

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <div className="w-full text-center text-4xl text-red-600 mt-8">
+                Home
+            </div>
+        </>
+    );
+};
+
+export default Home;
