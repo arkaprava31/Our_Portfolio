ff979550798f81a78bf7496fe8cc962fe201baa5
Arkaprava23 <arkaprava.digitalexpeditor@gmail.com>
2024-09-13 18:37:47 +0530

    hero

--- frontend/src/pages/Herosection.jsx
+++ frontend/src/pages/Herosection.jsx
@@ -38,26 +38,25 @@ const Herosection = () => {
 
 	return (
 		<div className="section-bg flex flex-col md:flex-row gap-4 items-center justify-center px-4 md:px-16 py-10 md:text-left text-center">
-			<div className="text-2xl md:text-4xl goldman-bold md:w-[543px]">
-				<div className="text-left md:px-0 px-6">
-					<h1 className="text-main  ">
-						A Team of Experts, Crafting
+			<div className="text-2xl md:text-4xl goldman-bold md:w-[543px] w-full flex flex-col items-start gap-2">
+				<div className="md:px-0 px-6 flex flex-col items-center gap-4">
+					<h1 className="text-main w-full text-left leading-[3rem]">
+						A Team of Experts, Crafting & Optimizing Web Solutions.
 					</h1>
-					<h1>& Optimizing Web Solutions.</h1>
-					<h2 className="text-2xl md:text-3xl">
+					<h2 className="text-2xl md:text-3xl w-full text-left">
 						We work on
 						<FlipWords words={words} className="text-[#8D75EE]" />
 					</h2>
 				</div>
 
-				<p className="text-sm text-gray-400 mt-6 mb-8">
+				<p className="text-sm text-gray-400 mt-6 mb-8 w-full text-left font-light tracking-wider leading-6">
 					A team of web developers collaborates to design, build, and maintain
 					websites and web applications, ensuring functionality, user
 					experience, and performance across digital platforms.
 				</p>
 				<button
 					onClick={handleScrollToProjects}
-					className="bg-[#8D75EE] text-white rounded-lg text-lg py-3 px-6 hover:bg-[#6D5DC9] transition-colors"
+					className="bg-[#8D75EE] text-white rounded-lg text-lg py-2.5 px-6 hover:bg-[#6D5DC9] transition-colors tracking-widest"
 				>
 					Let's Talk
 				</button>
@@ -66,7 +65,7 @@ const Herosection = () => {
 				<img
 					src="/images/HeroImage.png"
 					alt="Hero Section Image"
-					className="w-full h-auto w-full rounded-lg"
+					className="w-full h-auto rounded-lg"
 				/>
 			</div>
 		</div>
